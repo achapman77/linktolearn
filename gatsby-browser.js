7173ecbcc557870dd2c0ce6c47a9930533f3f731
Adam Chapman <adam@steeplinedesign.com>
2021-09-13 21:30:58 -0600

    remove preloader

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,26 +1,26 @@
-import "./src/styles/preloader.scss"
-// import { scrollToElement } from 'scroll-to-element'
+// import "./src/styles/preloader.scss"
+// // import { scrollToElement } from 'scroll-to-element'
 
 
-export const onRouteUpdate = ({ location }) => {
-  // console.info('onRouteUpdate =>')
-  // console.info({location})
-  checkHash(location)
-}
+// export const onRouteUpdate = ({ location }) => {
+//   // console.info('onRouteUpdate =>')
+//   // console.info({location})
+//   checkHash(location)
+// }
 
-const checkHash = location => {
-  // console.info('checkHash =>')
-  let { hash } = location
-  // console.info({location, hash, scrollToElement})
-  // var element = document.getElementById('sample_id');
+// const checkHash = location => {
+//   // console.info('checkHash =>')
+//   let { hash } = location
+//   // console.info({location, hash, scrollToElement})
+//   // var element = document.getElementById('sample_id');
 
-// document.body.style.overflow = 'visible';
-  if (hash) {
-    // scrollToElement(hash, {
-    //   offset: -80,
-    //   duration: 1000,
-    // })
-  }
-}
+// // document.body.style.overflow = 'visible';
+//   if (hash) {
+//     // scrollToElement(hash, {
+//     //   offset: -80,
+//     //   duration: 1000,
+//     // })
+//   }
+// }
 
-// export default onRouteUpdate
+// // export default onRouteUpdate
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,40 +1,40 @@
-import React from "react"
-import Layout from "./src/components/Layout"
-// import styled from "styled-components"
-import logo from './src/assets/images/logos/logo_main.png'
+// import React from "react"
+// import Layout from "./src/components/Layout"
+// // import styled from "styled-components"
+// import logo from './src/assets/images/logos/logo_main.png'
 
 
-const onRenderBody = ({
-  setPreBodyComponents,
-  setBodyAttributes,
-  setHeadComponents,
-  setPostBodyComponents
-}) => {
-  setHeadComponents([
-    <link as="script" rel="preload" href="/scripts/preloader.js" />,
-    <noscript>
-      <link rel="stylesheet" href="/styles/noscript.css" />
-    </noscript>
-  ])
-  setPreBodyComponents([
-    <div id="preloader">
-      {/* Optional: */}
-      <img src={logo} alt="logo" />
-      {/* <div className="preloader_animation"></div> */}
-      <div data-loader='500px-spinner'></div>
-    </div>
-  ])
-  setBodyAttributes({
-    className: "preloader_active"
-  })
-  setPostBodyComponents([
-    <script src="/scripts/preloader.js" />
-  ])
-}
+// const onRenderBody = ({
+//   setPreBodyComponents,
+//   setBodyAttributes,
+//   setHeadComponents,
+//   setPostBodyComponents
+// }) => {
+//   setHeadComponents([
+//     <link as="script" rel="preload" href="/scripts/preloader.js" />,
+//     <noscript>
+//       <link rel="stylesheet" href="/styles/noscript.css" />
+//     </noscript>
+//   ])
+//   setPreBodyComponents([
+//     <div id="preloader">
+//       {/* Optional: */}
+//       <img src={logo} alt="logo" />
+//       {/* <div className="preloader_animation"></div> */}
+//       <div data-loader='500px-spinner'></div>
+//     </div>
+//   ])
+//   setBodyAttributes({
+//     className: "preloader_active"
+//   })
+//   setPostBodyComponents([
+//     <script src="/scripts/preloader.js" />
+//   ])
+// }
 
-// Wraps every page in a component
-const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
-}
+// // Wraps every page in a component
+// const wrapPageElement = ({ element, props }) => {
+//   return <Layout {...props}>{element}</Layout>
+// }
 
-export {onRenderBody, wrapPageElement }
+// export {onRenderBody, wrapPageElement }
