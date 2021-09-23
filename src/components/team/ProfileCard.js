9992bb3e4157ffbbe91a05cd1098ff3deee2a3dc
Adam Chapman <adam@steeplinedesign.com>
2021-09-23 14:00:08 -0600

    team update

--- src/components/team/ProfileCard.js
+++ src/components/team/ProfileCard.js
@@ -15,7 +15,7 @@ const ProfileCard = ({data}) => {
                 <div>
                     <Name>{data.first_name} {data.last_name}</Name>
                     <Title>{data.position_title}</Title>
-                    <Title>{data.position_subTitle}</Title>
+                    <Title className="subTitle">{data.position_subTitle}</Title>
                 </div>
                 <StyledLink href={data.profile_url} aria-label="View our LinkedIn Profile">
                     <StyledIcon/>
@@ -36,8 +36,13 @@ const StyledGatsbyImage = styled(GatsbyImage)`
 `
 
 const Card = styled.div`
-    width: clamp(260px, 40vw, 450px);
-    /* padding: 2rem; */
+    width: clamp(260px, 15vw, 345px);
+    ${props => props.theme.lg`
+        flex-basis: 35vw;
+    `}
+    ${props => props.theme.sm`
+        flex-basis: 65vw;
+    `}
     .gatsby-image-wrapper {
         filter: grayscale(1) brightness(1);
         transition: 0.5s cubic-bezier(0.075,0.82,0.165,1);
@@ -73,6 +78,13 @@ const Title = styled.div`
     font-style: italic;
     padding-top: 0.5rem;
     letter-spacing: 0.25em;
+    &.subTitle {
+        font-size: clamp(0.65rem, 10vw, 0.75rem);
+        height: 1rem;
+        padding-top: 9px;
+        /* color: ${props => props.theme.colors.primary.main};
+        background: ${props => props.theme.colors.gray.dark};; */
+    }
 `
 
 const StyledLink = styled.a`
